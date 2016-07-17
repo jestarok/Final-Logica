fa774026fb4528d802d1c2023c75f433e5b61ce1
Jesus <jesus9528@gmail.com>
2016-07-17 14:41:03 -0400

    Creada la ventana de preguntas

--- principal.py
+++ principal.py
@@ -12,8 +12,37 @@ w = Label(master, text="FINAL")
 w.pack()
 
 def Test():
+    master = Toplevel()
 
+    contpregunta = IntVar()
+    contpregunta.set(0)
+    palabrassanguineas = []
+    palabrasmelancolicas = []
+    palabrascolericas = []
+    palabrasflematicas = []
+    preguntas = []
 
+    def next():
+        contpregunta.set(contpregunta.get()+1)
+        preguntalbl.config(text=contpregunta.get())
+
+    for pregunta in prolog.query("pregunta(P)"):
+        preguntas.append(pregunta["P"])
+
+    for palabras in prolog.query("palabras('C:/sanguineo.txt',PS)"):
+        palabrassanguineas.append(palabras["PS"])
+    for palabras in prolog.query("palabras('C:/colerico.txt',PS)"):
+        palabrascolericas.append(palabras["PS"])
+    for palabras in prolog.query("palabras('C:/melancolico.txt',PS)"):
+        palabrasmelancolicas.append(palabras["PS"])
+    for palabras in prolog.query("palabras('C:/flematico.txt',PS)"):
+        palabrasflematicas.append(palabras["PS"])
+
+    preguntalbl = Label(master, text=contpregunta.get())
+    preguntalbl.pack()
+    btn = Button(master,text="wii",command=lambda: next())
+    btn.pack()
+    preguntalbl.update_idletasks()
 
 b1 = Button(master,text="Tomar Test", command = Test)
 b2 = Button(master,text="Masa de Planetas")
