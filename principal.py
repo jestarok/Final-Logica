516ccbd9cc1612534df9bb44c35f18fcdd919f0f
Jesus <jesus9528@gmail.com>
2016-07-17 21:14:52 -0400

    Ya se calcula la personalidad Dominante

--- principal.py
+++ principal.py
@@ -2,11 +2,11 @@ import os
 os.chdir("C:\\Program Files (x86)\\swipl\\bin")
 from pyswip import *
 from Tkinter import *
-
+import operator
 master = Tk()
 master.minsize(500,500)
 prolog = Prolog()
-prolog.consult("C:/final.pl")
+prolog.consult("C:/prol/final.pl")
 
 
 def registro():
@@ -36,19 +36,46 @@ def Test(H,P):
     test.minsize(600,255)
     contpregunta = IntVar()
     contpregunta.set(0)
-    contsanguinea = 0
-    contcolericas = 0
-    contmelancolicas = 0
-    contflematicas = 0
+    contsanguinea = IntVar()
+    contsanguinea.set(0)
+    contcolericas =IntVar()
+    contcolericas.set(0)
+    contmelancolicas = IntVar()
+    contmelancolicas.set(0)
+    contflematicas = IntVar()
+    contflematicas.set(0)
     palabrassanguineas = []
     palabrasmelancolicas = []
     palabrascolericas = []
     palabrasflematicas = []
     preguntas = []
 
-    def next():
+    def sumarpuntos(numero):
+        print numero
+        if numero == 0:
+            contsanguinea.set(contsanguinea.get()+1)
+        elif numero == 1:
+            contcolericas.set(contcolericas.get()+1)
+        elif numero == 2:
+            contmelancolicas.set(contmelancolicas.get() +1)
+        elif numero == 2:
+            contflematicas.set(contflematicas.get()+1)
+
+    def maxindex():
+        list = [contsanguinea.get(),contcolericas.get(),contmelancolicas.get(),contflematicas.get()]
+        index, value = max(enumerate(list), key=operator.itemgetter(1))
+        if index == 0:
+            return "sanguineo"
+        elif index == 1:
+            return "colerico"
+        elif index == 2:
+            return "melancolico"
+        elif index == 2:
+            return "flematico"
+
+    def next(contador):
         if(contpregunta.get() == 14):
-            #resultado()
+            print maxindex()
             test.destroy()
         else:
             contpregunta.set(contpregunta.get()+1)
@@ -57,33 +84,38 @@ def Test(H,P):
             btn2.config(text=palabrascolericas[contpregunta.get()])
             btn3.config(text=palabrasmelancolicas[contpregunta.get()])
             btn4.config(text=palabrasflematicas[contpregunta.get()])
+            sumarpuntos(contador)
+            print contsanguinea.get(),contcolericas.get(),contmelancolicas.get(),contflematicas.get()
 
-
+#        maxi.set(max(contsanguinea.get(),contcolericas.get(),contmelancolicas.get(),contflematicas.get()))
 
 
     for pregunta in prolog.query("pregunta(P)"):
         preguntas.append(pregunta["P"])
 
-    for palabras in prolog.query("palabras('C:/sanguineo.txt',PS)"):
+    for palabras in prolog.query("palabras('C:/prol/sanguineo.txt',PS)"):
         palabrassanguineas.append(palabras["PS"])
-    for palabras in prolog.query("palabras('C:/colerico.txt',PS)"):
+    for palabras in prolog.query("palabras('C:/prol/colerico.txt',PS)"):
         palabrascolericas.append(palabras["PS"])
-    for palabras in prolog.query("palabras('C:/melancolico.txt',PS)"):
+    for palabras in prolog.query("palabras('C:/prol/melancolico.txt',PS)"):
         palabrasmelancolicas.append(palabras["PS"])
-    for palabras in prolog.query("palabras('C:/flematico.txt',PS)"):
+    for palabras in prolog.query("palabras('C:/prol/flematico.txt',PS)"):
         palabrasflematicas.append(palabras["PS"])
 
     preguntalbl = Label(test, text=preguntas[contpregunta.get()])
     preguntalbl.pack()
-    btn1 = Button(test,text=palabrassanguineas[contpregunta.get()],command=lambda: next())
+    btn1 = Button(test,text=palabrassanguineas[contpregunta.get()],command=lambda: next(0))
     btn1.pack(side=LEFT,padx=25)
-    btn2 = Button(test,text=palabrascolericas[contpregunta.get()],command=lambda: next())
+    btn2 = Button(test,text=palabrascolericas[contpregunta.get()],command=lambda: next(1))
     btn2.pack(side=LEFT,padx=25)
-    btn3 = Button(test,text=palabrasmelancolicas[contpregunta.get()],command=lambda: next())
+    btn3 = Button(test,text=palabrasmelancolicas[contpregunta.get()],command=lambda: next(2))
     btn3.pack(side=LEFT,padx=25)
-    btn4 = Button(test,text=palabrasflematicas[contpregunta.get()],command=lambda: next())
+    btn4 = Button(test,text=palabrasflematicas[contpregunta.get()],command=lambda: next(3))
     btn4.pack(side=LEFT,padx=25)
     preguntalbl.update_idletasks()
+
+
+
 
 #Mensaje de ayuda
 def Ayuda():
